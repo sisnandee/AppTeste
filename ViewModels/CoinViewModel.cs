d25e1b01cc76ec01b2e678079e2ad8a279c4ee99
etechas <etechas@LI02-06>
2025-09-27 11:58:00 -0300

    coisas nova3

--- ViewModels/CoinViewModel.cs
+++ ViewModels/CoinViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using AppTeste.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
 
@@ -10,13 +11,83 @@ namespace AppTeste.ViewModels
 {
     public partial class CoinViewModel : ObservableObject
     {
-        public string _LadoEscolhido = string.Empty;
-        public string _Imagem = string.Empty;
-        public string _Resultado = string.Empty;
-        public void Flip()
+        public CoinViewModel()
         {
-            Coin coin = new Coin();
-            _Resultado = coin.Jogar(_LadoEscolhido);
+            Application.Current.MainPage
+                .DisplayAlert("Mensagem", "Bem-Vindo(a) ao COIN FLIP!!", "OK");
+
+            FlipCommand = new Command(Flip);
         }
+        public ICommand FlipCommand { get; set; }
+
+        [ObservableProperty]
+        public string _LadoEscolhido = string.Empty;
+
+        [ObservableProperty]
+        public string _Imagem = string.Empty;
+
+        [ObservableProperty]
+        public string _Resultado = string.Empty;
+
+        public async void Flip()
+        {
+            try
+            {
+               
+
+                if (string.IsNullOrEmpty(_LadoEscolhido)) 
+                {
+                    throw new Exception("Selecione o lado da moeda");
+                }
+
+                string nome = await Application.Current.MainPage.
+                DisplayPromptAsync("Identificação", "Digite seu nome: ");
+
+                string diaSemana = 
+                await Application.Current.MainPage
+                    .DisplayActionSheet("Dia da Semana", 
+                    "Cancelar", 
+                    "OK",
+                    "Domingo",
+                    "Segunda",
+                    "Terça",
+                    "Quarta",
+                    "Quinta",
+                    "Sexta",
+                    "Sábado");
+
+                Coin coin = new Coin();
+                _Resultado = coin.Jogar(_LadoEscolhido);
+                _Imagem = $"{coin.Lado}.png";
+
+                _Resultado = $"{nome}, Hoje é {diaSemana}, {_Resultado}";
+
+                OnPropertyChanged(nameof(Resultado));
+                OnPropertyChanged(nameof(Imagem));
+
+                bool retorno= await Application.Current.MainPage
+                    .DisplayAlert("Mensagem", "Deseja reiniciar o jogo?", "Sim", "Não");
+
+                if(retorno)
+                {
+                    _Resultado = string.Empty;
+                    _Imagem = string.Empty;
+
+                    OnPropertyChanged(nameof(Resultado));
+                    OnPropertyChanged(nameof(Imagem));
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage
+                    .DisplayAlert("Mensagem", ex.Message, "OK");
+            }
+
+            
+
+        }
+
     }
 }
--- Views/CoinView.xaml.cs
+++ Views/CoinView.xaml.cs
@@ -1,9 +1,14 @@
+using AppTeste.ViewModels;
+
 namespace AppTeste.Views;
 
 public partial class CoinView : ContentPage
 {
+
 	public CoinView()
 	{
 		InitializeComponent();
+		this.BindingContext = new CoinViewModel();
 	}
+
 }
